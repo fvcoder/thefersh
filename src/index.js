4554f44298631b35490b1114e931524fd83d094f
Fernando Ticona <thefersh24@gmail.com>
2022-06-03 23:00:22 -0400

    add generator

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const Parser = require('rss-parser')
+const fs = require('fs')
+const path = require('path')
+const parser = new Parser()
+
+const getArticlesFromBlog = () =>
+  parser.parseURL('https://thefersh.com/feed.rss')
+
+
+async function main() {
+    const article = await getArticlesFromBlog()
+    const template = fs.readFileSync(path.join(__dirname, 'README.md.tpl')).toString()
+
+    const lastArticles = article.items.slice(0, 5)
+        .map(({ title, link }) => `- [${title}](${link})`)
+        .join('\n')
+
+   
+   const readme = template.replace('%{{latest_articles}}%', lastArticles)
+
+   fs.writeFileSync(path.join(__dirname, '../README.md'), readme)
+}
+
+main()
