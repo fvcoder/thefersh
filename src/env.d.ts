7349086d13be204094626aa0f0856ae7eccb9924
Fernando Ticona <thefersh24@gmail.com>
2024-10-02 11:06:01 -0400

    Index Page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+    otherLocals: {
+        NOTION_API_KEY: string;
+        NOTION_PAGE_ID: string;
+    };
+  }
+}
